450f9d3f31b935b5581610ac332d1c9fe9e6613c
YouBou <ikas2lite@gmail.com>
2023-05-21 20:11:32 +0900

    [refactor] 関数呼び出しにリファクタ

--- hello.go
+++ hello.go
@@ -12,14 +12,15 @@ func main() {
 	x := hello.Input("type a number")
 	n, err := strconv.Atoi(x)
 	if err != nil {
-		goto err
+		error()
+		return
 	}
 	for i := 1; i <= n; i++ {
 		t += 1
 	}
 	fmt.Println("total:", t)
-	return
+}
 
-err:
+func error() {
 	fmt.Println("ERROR!")
 }
