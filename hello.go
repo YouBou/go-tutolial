6d627811ddf2d3c3fffcc77325fac93d3cbd974e
YouBou <ikas2lite@gmail.com>
2023-05-25 21:49:15 +0900

    [feat] funcの利用

--- hello.go
+++ hello.go
@@ -5,12 +5,19 @@ import (
 )
 
 func main() {
-	m := map[string]int{
-		"a": 100,
-		"b": 200,
-		"c": 300,
-	}
-	for k, v := range m {
-		fmt.Println(k+":", v)
-	}
+	m := []string{}
+	m, _ = push(m, "apple")
+	m, _ = push(m, "banana")
+	m, _ = push(m, "orange")
+	fmt.Println(m)
+	m, v := pop(m)
+	fmt.Println("get "+v+" ->", m)
+}
+
+func push(a []string, v string) ([]string, int) {
+	return append(a, v), len(a)
+}
+
+func pop(a []string) ([]string, string) {
+	return a[:len(a)-1], a[len(a)-1]
 }
