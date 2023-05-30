d4f120d5d9621fb56a463a81830fc4073f42d513
YouBou <ikas2lite@gmail.com>
2023-05-30 22:09:27 +0900

    [feat] ポインタの利用

--- hello.go
+++ hello.go
@@ -5,8 +5,11 @@ import (
 )
 
 func main() {
+	// 整数値の代入
 	n := 123
-	b := true
-	s := "hello"
-	fmt.Printf("number:%d, bool:%t, string:%s.", n, b, s)
+	// nのポインタを代入
+	p := &n
+	fmt.Println("number:", n)
+	fmt.Println("pointer:", p) // アドレスの出力
+	fmt.Println("value:", *p)  // ポインタにある値の出力
 }
