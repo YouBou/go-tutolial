da5da8f2ce376fc21157a28269a12f3e7cafc859
YouBou <ikas2lite@gmail.com>
2023-05-31 23:06:19 +0900

    [feat] 構造体の利用

--- hello.go
+++ hello.go
@@ -4,16 +4,13 @@ import (
 	"fmt"
 )
 
-func main() {
-	ar := []int{10, 20, 30}
-	fmt.Println(ar)
-	initial(&ar)
-	fmt.Println(ar)
+var mydata struct {
+	Name string
+	Data []int
 }
 
-func initial(ar *[]int) {
-	for i := 0; i < len(*ar); i++ {
-		// スライスの指定したインデックスのポインタではなくスライス自身のポインタを参照する
-		(*ar)[i] = 0
-	}
+func main() {
+	mydata.Name = "Taro"
+	mydata.Data = []int{10, 20, 30}
+	fmt.Println(mydata)
 }
