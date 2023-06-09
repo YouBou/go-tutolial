ac9b83b0ef4adfa825fcd0c56928b0e9e1db1d4f
YouBou <ikas2lite@gmail.com>
2023-06-09 23:47:53 +0900

    [feat] reflectの型チェック利用(配列)

--- hello.go
+++ hello.go
@@ -17,14 +17,14 @@ type GData interface {
 // NData is structure.
 type NData struct {
 	Name string
-	Data int
+	Data []int
 }
 
 // Set is NData method.
 func (nd *NData) Set(nm string, g General) GData {
 	nd.Name = nm
-	if reflect.TypeOf(g).Kind() == reflect.Int {
-		nd.Data = g.(int)
+	if reflect.TypeOf(g) == reflect.SliceOf(reflect.TypeOf(0)) {
+		nd.Data = g.([]int)
 	}
 	return nd
 }
@@ -37,14 +37,14 @@ func (nd *NData) Print() {
 // SData is structure
 type SData struct {
 	Name string
-	Data string
+	Data []string
 }
 
 // Set is SData method.
 func (sd *SData) Set(nm string, g General) GData {
 	sd.Name = nm
-	if reflect.TypeOf(g).Kind() == reflect.String {
-		sd.Data = g.(string)
+	if reflect.TypeOf(g) == reflect.SliceOf(reflect.TypeOf("")) {
+		sd.Data = g.([]string)
 	}
 	return sd
 }
@@ -56,8 +56,8 @@ func (sd *SData) Print() {
 
 func main() {
 	var data = []GData{}
-	data = append(data, new(NData).Set("Taro", 123))
-	data = append(data, new(SData).Set("Jiro", "hello!"))
+	data = append(data, new(NData).Set("Taro", []int{1, 2, 3}))
+	data = append(data, new(SData).Set("Jiro", []string{"hello", "bye"}))
 	data = append(data, new(NData).Set("Hanako", "98700"))
 	data = append(data, new(SData).Set("Sachiko", "happy?"))
 	for _, ob := range data {
