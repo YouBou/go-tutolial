645e55e10af40229ede8dacc55d4e7ebad712e14
YouBou <ikas2lite@gmail.com>
2023-06-10 17:16:30 +0900

    [fix] 変数のスレッド間共有

--- hello.go
+++ hello.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
-func hello(s string, n int) {
-	for i := 1; i <= 10; i++ {
-		fmt.Printf("<%d %s>\n", i, s)
+func main() {
+	msg := "start"
+	prmsg := func(nm string, n int) {
+		fmt.Println(nm, msg)
 		time.Sleep(time.Duration(n) * time.Millisecond)
 	}
-}
-
-func main() {
-	go hello("hello", 50)
-	hello("bye!", 100)
+	hello := func(n int) {
+		const nm string = "hello"
+		for i := 1; i < 10; i++ {
+			msg += " h" + strconv.Itoa(i)
+			prmsg(nm, n)
+		}
+	}
+	main := func(n int) {
+		const nm string = "*main"
+		for i := 0; i < 5; i++ {
+			msg += " m" + strconv.Itoa(i)
+			prmsg(nm, 100)
+		}
+	}
+	go hello(60)
+	main(100)
 }
