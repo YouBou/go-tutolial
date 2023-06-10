fcf72866cff3b83ed24805a4190d0df8572ac025
YouBou <ikas2lite@gmail.com>
2023-06-10 17:29:49 +0900

    [fix] チャンネルへ複数値の追加

--- hello.go
+++ hello.go
@@ -7,13 +7,16 @@ import (
 func total(n int, c chan int) {
 	t := 0
 	for i := 1; i <= n; i++ {
-		t += 1
+		t += i
 	}
 	c <- t
 }
 
 func main() {
 	c := make(chan int)
+	go total(1000, c)
 	go total(100, c)
-	fmt.Println("total:", <-c)
+	go total(10, c)
+	x, y, z := <-c, <-c, <-c
+	fmt.Println(x, y, z)
 }
