339a0d00a27d348d480db56c9c4ff3a144816eab
YouBou <ikas2lite@gmail.com>
2023-06-13 23:50:46 +0900

    [fix] 複数ウィジェットの表示

--- hello.go
+++ hello.go
@@ -1,24 +1,19 @@
 package main
 
 import (
-	"sync"
-
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
-
-// SrData is structure
-type SrData struct {
-	msg string
-	mux sync.Mutex
-}
 
 func main() {
 	a := app.New()
 
 	w := a.NewWindow("Hello")
 	w.SetContent(
-		widget.NewLabel("Hello Fyne!"),
+		widget.NewVBox(
+			widget.NewLabel("Hello Fyne!"),
+			widget.NewLabel("This is sample application!"),
+		),
 	)
 
 	w.ShowAndRun()
