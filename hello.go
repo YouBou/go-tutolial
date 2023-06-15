c9dbcb4e33e5391c5de1e032804b8209038cfd36
YouBou <ikas2lite@gmail.com>
2023-06-15 22:30:23 +0900

    [fix] 選択リストウィジェットの利用

--- hello.go
+++ hello.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"strconv"
-
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -12,13 +10,14 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 	l := widget.NewLabel("Hello Fyne!")
-	s := widget.NewSlider(0.0, 100.)
-	b := widget.NewButton("Check", func() {
-		l.SetText("value: " + strconv.Itoa(int(s.Value)))
+	sl := widget.NewSelect([]string{
+		"Eins", "Twei", "Drei",
+	}, func(s string) {
+		l.SetText("selected: " + s)
 	})
 	w.SetContent(
 		container.NewVBox(
-			l, s, b,
+			l, sl,
 		),
 	)
 	w.ShowAndRun()
