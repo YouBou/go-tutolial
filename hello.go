91f0d62ba7db9752b17f5bc33d36dc7d495c2392
YouBou <ikas2lite@gmail.com>
2023-06-15 22:26:11 +0900

    [fix] スライダーウィジェットの利用

--- hello.go
+++ hello.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -10,19 +12,13 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 	l := widget.NewLabel("Hello Fyne!")
-	r := widget.NewRadioGroup(
-		[]string{"One", "Two", "Three"},
-		func(s string) {
-			if s == "" {
-				l.SetText("not selected.")
-			} else {
-				l.SetText("selected: " + s)
-			}
-		})
-	r.SetSelected("One")
+	s := widget.NewSlider(0.0, 100.)
+	b := widget.NewButton("Check", func() {
+		l.SetText("value: " + strconv.Itoa(int(s.Value)))
+	})
 	w.SetContent(
 		container.NewVBox(
-			l, r,
+			l, s, b,
 		),
 	)
 	w.ShowAndRun()
