d99beb89a8a4ef9a2da7564eb60d44f17beab3a8
YouBou <ikas2lite@gmail.com>
2023-07-02 22:07:22 +0900

    [fix] 自作ウィジェットの利用

--- hello.go
+++ hello.go
@@ -3,37 +3,51 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
+
+// MyEntry is custom entry.
+type MyEntry struct {
+	widget.Entry
+	entered func(e *MyEntry)
+}
+
+// NewMyEntry create MyEntry.
+func NewMyEntry(f func(e *MyEntry)) *MyEntry {
+	e := &MyEntry{}
+	e.ExtendBaseWidget(e)
+	e.entered = f
+	return e
+}
+
+// KeyDown is Keydown Event.
+func (e *MyEntry) KeyDown(key *fyne.KeyEvent) {
+	switch key.Name {
+	case fyne.KeyReturn, fyne.KeyEnter:
+		e.entered(e)
+	default:
+		e.Entry.KeyDown(key)
+	}
+}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 	l := widget.NewLabel("Hello Fyne!")
-	e := widget.NewEntry()
-	b := widget.NewButton("Click", func() {
-		dialog.ShowCustomConfirm("Enter message.", "OK",
-			"Cancel", e, func(f bool) {
-				if f {
-					l.SetText("typed: '" + e.Text + "'.")
-				} else {
-					l.SetText("no message...")
-				}
-			}, w,
-		)
+	e := NewMyEntry(func(e *MyEntry) {
+		s := e.Text
+		e.SetText("")
+		l.SetText("you type '" + s + "'.")
 	})
+
 	w.SetContent(
-		fyne.NewContainerWithLayout(
-			layout.NewBorderLayout(
-				nil, b, nil, nil,
-			),
-			l, b,
+		container.NewVBox(
+			l, e,
 		),
 	)
 	a.Settings().SetTheme(theme.LightTheme())
-	w.Resize(fyne.NewSize(350, 250))
+	w.Resize(fyne.NewSize(300, 100))
 	w.ShowAndRun()
 }
